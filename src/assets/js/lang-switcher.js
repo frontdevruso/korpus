9bb17253cceb64d03d346394ce62ac56815ea126
backdevruso <feedback0225@gmail.com>
2022-07-13 12:22:59 +0300

    tasks 13.07

--- src/assets/js/lang-switcher.js
+++ src/assets/js/lang-switcher.js
@@ -1,10 +1,24 @@
 if (document.querySelectorAll('[data-language-switcher]')) {
     const allLangSwitchers = document.querySelectorAll('[data-language-switcher]');
     allLangSwitchers.forEach(function(switcher) {
-        switcher.querySelectorAll('button').forEach(function(btn) {
+        let currentLang = switcher.querySelector('.lang-switcher__btn--current');
+        switcher.querySelectorAll('.lang-switcher__btn').forEach(function(btn) {
             btn.addEventListener('click', function() {
-                let currentLanguage = btn.getAttribute('data-language');
-                switcher.setAttribute('data-language-switcher', currentLanguage);
+                let langType = btn.getAttribute('data-language');
+                let switchedLanguage = currentLang.getAttribute('data-language');
+
+                currentLang.setAttribute('data-language', langType)
+                currentLang.innerHTML = langType;
+                btn.setAttribute('data-language', switchedLanguage)
+                btn.innerHTML = switchedLanguage;
+
+                currentLang.classList.add('zoom');
+                btn.classList.add('hide');
+                
+                setTimeout(function() {
+                    currentLang.classList.remove('zoom');
+                    btn.classList.remove('hide');
+                }, 300);
             });
         });
     });
